ae7ef3708befbcac93a3dd1415ae866b0262dc05
woldemst <72108511+woldemst@users.noreply.github.com>
2023-02-22 09:33:26 +0100

    09:33

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,14 +1,19 @@
 import React from "react";
 
 import Input from "../../shared/FormElements/Input";
-
 import './NewPlace.scss'
 
 
 export default function NewPlace(){
     return (
         <form className="place-form">
-            <Input element="input" type="text" />             
+            <Input 
+                element="input"
+                type="text"  
+                label="Title" 
+                validators={[]} 
+                errorText={'Please enter a valid title.'} 
+            />             
         </form>
     )
 }
