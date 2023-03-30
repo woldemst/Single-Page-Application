51d51db9e41f3c6dfd2dc0cfc53e69c9136a32e5
woldemst <72108511+woldemst@users.noreply.github.com>
2023-03-30 10:53:34 +0200

    Fixed httpHook and used in Auth.js | connection of front- and backend

--- client/src/shared/hooks/http-hook.js
+++ client/src/shared/hooks/http-hook.js
@@ -1,52 +1,56 @@
-import { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback, useRef, useEffect } from 'react';
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
-  const activeHttpRequest = useRef([])
+  const activeHttpRequests = useRef([]);
 
-  const sendRequest = useCallback( async (
-    url,
-    method = "GET",
-    body = null,
-    headers = {}
-  ) => {
-    setIsLoading(true) 
+  const sendRequest = useCallback(
+    async (url, method = 'GET', body = null, headers = {}) => {
+      setIsLoading(true);
+      const httpAbortCtrl = new AbortController();
+      activeHttpRequests.current.push(httpAbortCtrl);
 
-    const httpAbortCtrl = new AbortController()
-    activeHttpRequest.current.push(httpAbortCtrl)
-    try{
+      try {
         const response = await fetch(url, {
           method,
           body,
           headers,
           signal: httpAbortCtrl.signal
         });
-    
+
         const responseData = await response.json();
-    
-        if(!response.ok){
-            throw new Error(responseData.message)
+
+        activeHttpRequests.current = activeHttpRequests.current.filter(
+          reqCtrl => reqCtrl !== httpAbortCtrl
+        );
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
         }
 
-        return responseData
+        setIsLoading(false);
+        return responseData;
+      } catch (err) {
+        setError(err.message);
+        setIsLoading(false);
+        throw err;
+      }
+    },
+    []
+  );
 
-    }catch(err){
-        setError(err.message)
-    }
-    setIsLoading(false)
+  const clearError = () => {
+    setError(null);
+  };
+
+  useEffect(() => {
+    return () => {
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      activeHttpRequests.current.forEach(abortCtrl => abortCtrl.abort());
+    };
   }, []);
 
-  const clearError = () =>{
-    setError(null)
-  }
-
-  useEffect(()=>{
-    return () => {
-        activeHttpRequest.current.forEach(abortCtrl => abortCtrl.abort())
-    }
-  })
-
-  return {isLoading, error, sendRequest, clearError}
+  return { isLoading, error, sendRequest, clearError };
 };
