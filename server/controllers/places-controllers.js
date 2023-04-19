5004b6fda34a37ff6b4dca25e64e0feecca7ebe1
woldemst <adlerrein@gmail.com>
2023-04-19 20:58:20 +0200

    Loging in with the hashed password | used bcryptjs package | Adding authentification

--- server/controllers/places-controllers.js
+++ server/controllers/places-controllers.js
@@ -109,7 +109,7 @@ const createPlace = async (req, res, next) => {
     return next(error);
   }
 
-  console.log(user);
+  // console.log(user);
 
   try {
     const sess = await mongoose.startSession();
--- server/controllers/users-controllers.js
+++ server/controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bycrypt = require("bcryptjs");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -48,11 +49,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let haahedPassword;
+  try {
+    haahedPassword = bycrypt.hash(password, 12);
+  } catch (err) {
+    const error = new HttpError("Could not create user,please try again", 500);
+    return next(error);
+  }
+
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: haahedPassword,
     places: [],
   });
 
@@ -77,10 +86,21 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
       401
+    );
+    return next(error);
+  }
+
+  let isValidPassword = false;
+  try {
+    isValidPassword = await bycrypt.compare(password, existingUser.password);
+  } catch (err) {
+    const error = new HttpError(
+      "Could not log you in, please check your credentials and try again.",
+      500
     );
     return next(error);
   }
