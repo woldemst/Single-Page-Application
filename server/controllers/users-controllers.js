bad465ad2c25aba8c086fb9800cb700cbb6e4bd5
woldemst <adlerrein@gmail.com>
2023-04-19 21:17:57 +0200

    Generating tokens for signing in  | Adding authentification

--- server/controllers/users-controllers.js
+++ server/controllers/users-controllers.js
@@ -1,5 +1,6 @@
 const { validationResult } = require("express-validator");
 const bycrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const HttpError = require("../models/http-error");
 const User = require("../models/user");
@@ -72,7 +73,19 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ user: createdUser.toObject({ getters: true }) });
+  let token;
+  try {
+    jwt.sign(
+      { userId: createdUser.id, email: createdUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Signing up failed, please try again.", 500);
+    return next(error);
+  }
+
+  res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
