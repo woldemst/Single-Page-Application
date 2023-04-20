3d8c6f97271edf4e49cb428b4cb6a37f51ab56d3
woldemst <adlerrein@gmail.com>
2023-04-20 17:59:21 +0200

    Generating tokens for loging in  | Adding authentification

--- server/controllers/users-controllers.js
+++ server/controllers/users-controllers.js
@@ -54,7 +54,7 @@ const signup = async (req, res, next) => {
   try {
     haahedPassword = bycrypt.hash(password, 12);
   } catch (err) {
-    const error = new HttpError("Could not create user,please try again", 500);
+    const error = new HttpError("Could not create user, please try again", 500);
     return next(error);
   }
 
@@ -85,7 +85,9 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
-  res.status(201).json({ userId: createdUser.id, email: createdUser.email, token: token });
+  res
+    .status(201)
+    .json({ userId: createdUser.id, email: createdUser.email, token: token });
 };
 
 const login = async (req, res, next) => {
@@ -118,9 +120,22 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
+  let token;
+  try {
+    jwt.sign(
+      { userId: existingUser.id, email: existingUser.email },
+      "supersecret_dont_share",
+      { expiresIn: "1h" }
+    );
+  } catch (err) {
+    const error = new HttpError("Loging in failed, please try again.", 500);
+    return next(error);
+  }
+
   res.json({
-    message: "Logged in!",
-    user: existingUser.toObject({ getters: true }),
+    userId: existingUser.id,
+    email: existingUser.email,
+    token: token,
   });
 };
 
